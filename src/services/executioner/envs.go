02c968afa3f102f4684e0034443e1562310f8099
ranon-rat <66473662+ranon-rat@users.noreply.github.com>
2026-01-01 15:58:35 -0600

    enviroment fix

--- src/services/executioner/envs.go
+++ src/services/executioner/envs.go
@@ -1,18 +1,21 @@
 package executionerServices
 
 import (
+	"fmt"
 	"os"
 	"slices"
+	"strings"
 )
 
 func LoadLocalEnviroments() []string {
 	ourEnvs := os.Environ()
 	envs := make([]string, 0, len(ourEnvs))
 	for _, env := range ourEnvs {
-		if slices.Contains([]string{"PORT", "PASSWORD"}, env) {
+		if slices.Contains([]string{"PORT", "PASSWORD"}, strings.Split(env, "=")[0]) {
 			continue
 		}
 		envs = append(envs, env)
 	}
+	fmt.Println(envs)
 	return envs
 }
