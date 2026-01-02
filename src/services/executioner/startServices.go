2ebbb3b7cb727ff1e692278ae6ef41bb08ca306b
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2026-01-02 02:19:10 -0600

    Update startServices.go

--- src/services/executioner/startServices.go
+++ src/services/executioner/startServices.go
@@ -90,7 +90,7 @@ func Executioner(project *projectsD.Project) {
 		runningProjects.Delete(project.ID)
 	}
 	ctx, cancel := context.WithCancel(context.Background())
-	cmd := exec.CommandContext(ctx, "bash", "-lc", "trap 'kill 0' SIGTERM; "+project.Command)
+	cmd := exec.CommandContext(ctx, "bash", "-lc" /*"trap 'kill 0' SIGTERM; "+*/, project.Command)
 	cmd.Dir = project.Dir
 	cmd.Env = executableEnv
 
