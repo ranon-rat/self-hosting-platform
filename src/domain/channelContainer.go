69bc7a084e6a4eed8d3fd4960405d515b677c85a
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2026-01-02 04:25:55 -0600

    Update channelContainer.go

--- src/domain/channelContainer.go
+++ src/domain/channelContainer.go
@@ -12,7 +12,10 @@ func NewSecureChanneling[T any](maxBuffer int) *SecureChanneling[T] {
 	return &SecureChanneling[T]{
 		ch: channel,
 	}
+
 }
+
+// this allows a more secure execution, since the channels can be reused(for practical usages)
 func (sc *SecureChanneling[T]) Close() {
 	sc.once.Do(func() {
 		close(sc.ch)
@@ -22,15 +25,20 @@ func (sc *SecureChanneling[T]) Close() {
 func (sc *SecureChanneling[T]) Send(v T) {
 	sc.ch <- v
 }
+
 func (sc *SecureChanneling[T]) Receive() T {
 	return <-sc.ch
 }
+
+// it adds to the buffer, if its not consumed it ignores that.
 func (sc *SecureChanneling[T]) AppendSend(v T) {
 	select {
 	case sc.ch <- v:
 	default:
 	}
 }
+
+// it works in the same way sync.Map ranges work.  the output must be true for it to continue
 func (sc *SecureChanneling[T]) Range(f func(T) bool) {
 	for out := range sc.ch {
 		if !f(out) {
